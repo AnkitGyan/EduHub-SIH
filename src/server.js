1b0e19dbd33c7ce059e80828e22358797bca45d6
Ankit <ankit0525252@gmail.com>
2025-09-15 20:00:14 +0530

    initialization of project

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+import connectDB from "./config/db.js";
+
+
+
+dotenv.config();
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+
+// socket.io
+chatSocket(io);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
