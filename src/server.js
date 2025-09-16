f0ca12132a1b1033ea1f008fb6705a330a23475c
Ankit <ankit0525252@gmail.com>
2025-09-17 01:30:24 +0530

    completed student info dashboard and social interaction

--- src/server.js
+++ src/server.js
@@ -1,24 +1,21 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import studentRoutes from "./routes/studentRoutes.js";
-import simulationRoutes from "./routes/simulationRoutes.js";
-import quizRoutes from "./routes/quizRoutes.js";
-import socialRoutes from "./routes/socialRoutes.js";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import  {connectDB} from "./config/db.js"
 
 dotenv.config();
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
-app.use("/api/students", studentRoutes);
-app.use("/api/simulations", simulationRoutes);
-app.use("/api/quizzes", quizRoutes);
-app.use("/api/social", socialRoutes);
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+// connect DB
+connectDB();
 
 const PORT = process.env.PORT || 5000;
-
-connectDB().then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
